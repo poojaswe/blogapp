9143ebc3c363b9451fd78070db20ebec179ab555
Pooja SWE <158512520+poojaswe@users.noreply.github.com>
2024-02-08 22:07:02 +0000

    added route and links

--- client/src/components/header/index.jsx
+++ client/src/components/header/index.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import classes from "./styles.module.css";
 
 export default function Header() {
@@ -5,8 +6,11 @@ export default function Header() {
         <div className={classes.header}>
             <h1>BLOG APP</h1>
             <ul>
-                <li>Blogs</li>
-                <li>Add Blog</li>
+                <Link to={'/'}>
+                    <li>Blogs</li></Link>
+                <Link to={'/add-blog'}>
+                    <li>Add Blog</li>
+                </Link>
             </ul>
         </div>
     );
--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import GlobalState from './context';
-
+import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <GlobalState>
-    <App />
-  </GlobalState>
+  <BrowserRouter>
+    <GlobalState>
+      <App />
+    </GlobalState>
+  </BrowserRouter>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
